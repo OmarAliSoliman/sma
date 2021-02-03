aec12378cc0939772db8524d998401701689969e
OmarAliSoliman <omarasmar9955@gmail.com>
2021-02-03 03:26:13 +0200

    first

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,48 @@
 $(document).ready(function () {
   
+  if ($(".sma-team-slider").length) {
+    $(".sma-team-slider").slick({
+      dots: false,
+      // centerMode: true,
+      // centerPadding: '60px',
+      focusOnSelect: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      loop: true,
+      arrows: true,
+      infinite: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 524,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+          },
+        },
+      ],
+    });
+  }
+
 });
 
